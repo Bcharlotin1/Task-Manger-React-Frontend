5d8eb38300fe085b44660d763afb9f39a6da8e91
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 14:55:02 -0500

    TaskInputes  updated

--- src/components/tasks/TaskInputs.js
+++ src/components/tasks/TaskInputs.js
@@ -1,36 +1,37 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
-import { createProject } from "../../actions/projectAction"
+import { createTask } from "../../actions/taskAction"
 import { useNavigate } from "react-router"
-import '/Users/biancacharlotin/Development/code/React-App/task-manager-frontend-copy/src/Form.css'
 
-export default function ProjectInput() {
-    
+
+export default function TaskInputs() {
     const [title, setTitle] = useState("")
-    
-    const [description, setDescription] = useState("")
+    //retuns ana array of 2 item  1.  inital stat 2.  funtion to change that value
+   
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
-    console.log(description)
+ 
 
     function handleSubmit(event){
         event.preventDefault()
-     
-        dispatch(createProject({
+        
+        dispatch(createTask({
             title: title,
-            description: description,
-            completion_rate: 0
+       
+            
+            //completion_rate ussseeeffeect to get all task,  finnd the tasskees for  a particular  projeect, then devide them by 100 to distributee precentages,  eavee tasj chould have the same %
+
+            //project id neees to bee sent through th get associated may be able to get once i determin dynamic routes neeed to send an whol projeect  objact to the back end 
         }, navigate))
 
         setTitle("")
-        setDescription("")
+        
     }
     return (
         <div>
-             <div className="projects-section">
+              <div className="projects-section">
               <div className="projects-section-header">
-              <p>New Project</p>
+              <p>New Task</p>
             
               <p className="time">December 12</p>
             
@@ -42,35 +43,33 @@ export default function ProjectInput() {
             
               </button>
           
+              
               </div>
               
               </div>
-              <div>
-                <form onSubmit={handleSubmit}>
-                    <label>Title</label>
-                    <input 
-                    type="text" 
-                    
-                    placeholder="Your title.." 
-                    value={title}
-                    onChange={(event) => setTitle(event.target.value)}/>
-                    
+            <form onSubmit={handleSubmit}>
+            
+                <label>Title</label>
+                <input 
+                type="text"
+                placeholder="Your title.."
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}>
 
-                    <label>Description</label>
-                    <textarea
-                        placeholder="Write something.."
-                        style={{ height: 200 }}
-                        value={description}
-                        onChange={(event) => setDescription(event.target.value)}
-                    />
-                    <input type="submit"></input>
-                `</form>
-                </div>
-            </div>
+                </input>
+
+              
+
+                <input type="submit"></input>
+            </form>
+        </div>
         </div>
     )
     
 }
+
+
+
 
 
 
