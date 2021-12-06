4d60eda75be6e0ca22592349f2a4fcb682c33f50
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 14:40:29 -0500

    Home page has the links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+              <h1>Welcome</h1>
+   
+            <Link to='/login'>Login</Link>{"  "}
+            <Link to='/signup'>Signup</Link>
         </div>
     )
 }
