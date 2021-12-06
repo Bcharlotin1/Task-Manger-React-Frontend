2a6116740fb6d4030a06a19e6acc07d687677335
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 15:13:21 -0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
 import React from 'react';
+import PageLayout from './components/layout/PageLayout';
 import './App.css';
 
-
-import UserDisplay from './components/users/UserDisplay';
-import UserSignup from './components/users/UserSignup';
-import UserLogin from './components/users/UserLogin';
-import ProjectInput from './components/projects/ProjectInput';
-import ProjectDisplay from './components/projects/ProjectDisplay';
-import TaskDisplay from './components/tasks/TaskDisplay';
-import TaskInputs from './components/tasks/TaskInputs';
 import Home from './components/Home';
-import { Link, Route, Routes } from 'react-router-dom';
-import Nav from './components/SideNav/Nav';
+import UserLogin from './components/users/UserLogin'
+import UserSignup from './components/users/UserSignup'
+import UserDisplay from './components/users/UserDisplay';
 
-import DisplayTest from './components/projects/DisplayTest';
+
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div >
-      <h1>Welcome</h1>
-      <Nav />
-      <Link to='/signup'>Signup</Link>
-      <Link to='/login'>Login</Link>
+    
+  
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
-        <Route path="/login" element={<UserLogin />} />
         <Route path="/user" element={<UserDisplay />} />
-        <Route path="/projects" element={<ProjectDisplay />} />
-       
-        <Route path="/projects/:id" element={<ProjectDisplay />} />
-        <Route path="/projects/:id" element={<DisplayTest />} />
-
-        <Route path="/projects/new" element={<ProjectInput />} />
-        <Route path="/projects/tasks" element={<TaskDisplay />} />
-        <Route path="/projects/tasks/new" element={<TaskInputs />} />
-
-     
-        
-        {/* <Route path="/signup" element={<UserSignup />} />
-        <Route path="/login" element={<UserLogin />} />
-        <Route path="/user" element={<UserDisplay />} />
-        <Route path="/projects" element={<ProjectDisplay />} />
-          <Route path="/projects/new" element={<ProjectInput />} />
-         
-        <Route path="/projects/new" element={<ProjectInput />} /> */}
-      
-       
+        <Route path="/projects" element={<PageLayout />} />
+        <Route path="/projects/new" element={<PageLayout />} />
+        <Route path="/projects/tasks" element={<PageLayout />} />
+        <Route path="/projects/tasks/new" element={<PageLayout />} />
       </Routes>
-
- 
     </div>
   );
 }
 
 export default App;
+
