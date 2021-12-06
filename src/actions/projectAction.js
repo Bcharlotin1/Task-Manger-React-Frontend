39f5d8c260589039dace356cc5fc579338a8c855
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 15:09:38 -0500

    ProjectAction updated

--- src/actions/projectAction.js
+++ src/actions/projectAction.js
@@ -1,11 +1,11 @@
-import { SET_PROJECTS, ADD_PROJECT, DELETE_PROJECT, UPDATE_PROJECT } from "./constants";
+import { SET_PROJECTS, ADD_PROJECT, DELETE_PROJECT } from "./constants";
 
 export function fetchProjects(){
     return dispatch => {
         fetch('http://localhost:3000/projects')
         .then(r => r.json())
         .then( projects => { 
-           
+         
             dispatch({type:SET_PROJECTS, payload: projects})})
     }
 }
@@ -31,29 +31,13 @@ export function createProject(project, navigate){
 export function deleteProject(project, navigate){
     return dispatch => {
         fetch(`http://localhost:3000/projects/${project.id}`,{
-            method: "DELETE",
+            method: "DELETE"  
         })
-        .then(r => { 
-            console.log(r)
-            dispatch({type:DELETE_PROJECT, payload: project})})
-            navigate("/projects")
-    }
-}
-
-export function editProject(project, navigate){
-    return dispatch => {
-        fetch(`http://localhost:3000/projects/${project.id}`,{
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(project)
-        })
-        .then(r => r.json())
+        .then(r => {r.json()})
         .then( project => { 
-            
-            dispatch({type:UPDATE_PROJECT, payload: project})})
+    
+            console.log(project)
+            dispatch({type:DELETE_PROJECT, payload: project})})
             navigate("/projects")
     }
 }
