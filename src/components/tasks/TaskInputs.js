c6f9f58d10b72caa112426a64265819683ab0d9f
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 14:54:07 -0500

    TaskInputes  updated

--- src/components/tasks/TaskInputs.js
+++ src/components/tasks/TaskInputs.js
@@ -1,37 +1,36 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
-import { createTask } from "../../actions/taskAction"
+import { createProject } from "../../actions/projectAction"
 import { useNavigate } from "react-router"
+import '/Users/biancacharlotin/Development/code/React-App/task-manager-frontend-copy/src/Form.css'
 
-
-export default function TaskInputs() {
+export default function ProjectInput() {
+    
     const [title, setTitle] = useState("")
-    //retuns ana array of 2 item  1.  inital stat 2.  funtion to change that value
-   
+    
+    const [description, setDescription] = useState("")
     const dispatch = useDispatch()
     const navigate = useNavigate()
- 
+
+    console.log(description)
 
     function handleSubmit(event){
         event.preventDefault()
-        
-        dispatch(createTask({
+     
+        dispatch(createProject({
             title: title,
-       
-            
-            //completion_rate ussseeeffeect to get all task,  finnd the tasskees for  a particular  projeect, then devide them by 100 to distributee precentages,  eavee tasj chould have the same %
-
-            //project id neees to bee sent through th get associated may be able to get once i determin dynamic routes neeed to send an whol projeect  objact to the back end 
+            description: description,
+            completion_rate: 0
         }, navigate))
 
         setTitle("")
-        
+        setDescription("")
     }
     return (
         <div>
-              <div className="projects-section">
+             <div className="projects-section">
               <div className="projects-section-header">
-              <p>New Task</p>
+              <p>New Project</p>
             
               <p className="time">December 12</p>
             
@@ -43,30 +42,36 @@ export default function TaskInputs() {
             
               </button>
           
-              
               </div>
               
               </div>
-            <form onSubmit={handleSubmit}>
-            
-                <label>Title</label>
-                <input 
-                type="text"
-                placeholder="Your title.."
-                value={title}
-                onChange={(event) => setTitle(event.target.value)}>
+              <div>
+                <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input 
+                    type="text" 
+                    
+                    placeholder="Your title.." 
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}/>
+                    
 
-                </input>
-
-              
-
-                <input type="submit"></input>
-            </form>
-        </div>
+                    <label>Description</label>
+                    <textarea
+                        placeholder="Write something.."
+                        style={{ height: 200 }}
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                    <input type="submit"></input>
+                `</form>
+                </div>
+            </div>
         </div>
     )
     
 }
+
 
 
 
