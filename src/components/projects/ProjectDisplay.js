4e3d44cf1a7a5b398d606c06ca5464b4cedbbd66
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 14:58:46 -0500

    ProjectDisplay update

--- src/components/projects/ProjectDisplay.js
+++ src/components/projects/ProjectDisplay.js
@@ -1,49 +1,74 @@
-import { useEffect} from "react"
-import { Link} from "react-router-dom"
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchProjects, deleteProject} from '../../actions/projectAction'
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { createProject } from "../../actions/projectAction"
 import { useNavigate } from "react-router"
+import '/Users/biancacharlotin/Development/code/React-App/task-manager-frontend-copy/src/Form.css'
 
-
-export default function ProjectDisplay() {
-  
-
-    const projects = useSelector(state => state.projects)
+export default function ProjectInput() {
+    
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    useEffect(() =>{
-        dispatch(fetchProjects())
-    }, [])
+    console.log(description)
 
-    function handleClick(project){
-        
-        dispatch(deleteProject(project, navigate))
+    function handleSubmit(event){
+        event.preventDefault()
+     
+        dispatch(createProject({
+            title: title,
+            description: description,
+            completion_rate: 0
+        }, navigate))
+
+        setTitle("")
+        setDescription("")
     }
-   
-    
-   
-    
-   return(
-       <div>
-            <h1>Projects</h1>
-            <ul>
+    return (
+        <div>
+             <div className="projects-section">
+              <div className="projects-section-header">
+              <p>New Project</p>
             
-                {projects.map(p =>{
-                return(
-                    <li key={p.id}>
-                        
-                        
-                        <p>{p.description}</p>
-                        <p>{p. completion_rate}</p>
-                        <button onClick={()=>{handleClick({p})}}>Delete</button>
-                                    {/* <button onClick={()=>{handleEdit(p)}}>Edit</button> */}
-                                    {/* <Outlet /> */}
-                                    {/* //render any matching child you have ,set itn the route in app js */}
-                    </li>
-                )})}
-            </ul>
-        </div>             
-        )
+              <p className="time">December 12</p>
+            
+              </div>
+              <div className="projects-section-line">
+         
+              <div className="view-actions">
+              <button className="view-btn list-view" title="List View">
+            
+              </button>
+          
+              </div>
+              
+              </div>
+              <div>
+                <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input 
+                    type="text" 
+                    
+                    placeholder="Your title.." 
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}/>
+                    
+
+                    <label>Description</label>
+                    <textarea
+                        placeholder="Write something.."
+                        style={{ height: 200 }}
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                    />
+                    <input type="submit"></input>
+                `</form>
+                </div>
+            </div>
+        </div>
+    )
+    
 }
+
 
