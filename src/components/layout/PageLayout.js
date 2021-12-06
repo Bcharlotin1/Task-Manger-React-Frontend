14f7bb82be07b851adf1ceef9d45eae13aafdb70
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 15:05:05 -0500

    layout folder

---
+++ src/components/layout/PageLayout.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import SideNavLayout from './SideNavLayout';
+import logo from '../../images/logo3.png'; // with import
+
+import ProjectDisplay from '../projects/ProjectDisplay';
+import ProjectInput from '../projects/ProjectInput'
+import TaskDisplay from '../tasks/TaskDisplay'
+import TaskInput from '../tasks/TaskInputs'
+import { useLocation, matchPath } from 'react-router-dom';
+
+export default function PageLayout() {
+    
+  const {pathname} = useLocation()
+  // const match =  matchPath("/projects/*/task", {
+  //   path: "/projects/:id/task",
+  //   exact: true,
+  //   strict: false
+  // });
+
+  function headerDisplay(){
+    if (pathname === "/projects" || pathname === "/projects/") {
+      return (<ProjectDisplay />)
+    }
+    if(pathname === "/projects/new"){
+      return (<ProjectInput />)
+    }
+    if(pathname === "/projects/tasks"){
+  
+      return (<TaskDisplay />)
+    }
+    if(pathname === "/projects/tasks/new"){
+      return (<TaskInput />)
+    }
+  }
+  
+  
+  
+  return (
+    <div className="app-container">
+    <div className="app-header">
+      <div className="app-header-left">
+        {/* <span className="app-icon" /> */}
+       <div className="app-icon"><img src={logo}/></div>
+    
+        <span></span>
+        <p className="app-name"></p>
+        <div className="search-wrapper">
+        <p className="app-name">Task Manager App</p>
+          {/* <input className="search-input" type="text" placeholder="Search" />
+          <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="feather feather-search" viewBox="0 0 24 24">
+            <defs />
+            <circle cx={11} cy={11} r={8} />
+            <path d="M21 21l-4.35-4.35" />
+          </svg> */}
+        </div>
+      </div>
+      <div className="app-header-right">
+
+        <button className="profile-btn">
+          <img src="https://assets.codepen.io/3306515/IMG_2025.jpg" />
+          <span>Aybüke C.</span>
+        </button>
+      </div>
+      
+    </div>
+    <div className="app-content">
+        <SideNavLayout />
+    
+          {headerDisplay()}
+      
+        {/* ---------------------------------- */}
+  
+      
+    </div>
+    
+    </div>
+     
+);
+}
+
---
+++ src/components/layout/SideNavLayout.js
@@ -0,0 +1,29 @@
+import { Link, NavLink } from 'react-router-dom';
+
+export default function SideNavLayout() {
+    return (
+       <div className="app-sidebar">
+        
+            <NavLink to='/projects' className="app-sidebar-link">
+                <svg className="link-icon" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
+                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    <polyline points="9 22 9 12 15 12 15 22" />
+                </svg>
+            </NavLink>
+         
+            <NavLink to='/projects/tasks' className="app-sidebar-link">
+  
+                <svg className="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
+                </svg>
+            </NavLink>
+
+            
+            <a href className="app-sidebar-link">
+            <svg className="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 17l5-5-5-5M19.8 12H9M10 3H4v18h6"/></svg>
+            </a>
+
+
+            
+      </div>
+    )
+}
