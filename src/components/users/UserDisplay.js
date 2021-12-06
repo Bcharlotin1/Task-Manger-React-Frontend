c63d29d6137094f48acb8c5b6b26b18395ef16f9
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-06 14:47:53 -0500

    UserDisplay updated

--- src/components/users/UserDisplay.js
+++ src/components/users/UserDisplay.js
@@ -1,23 +1,29 @@
-// import {  useEffect} from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { fetchUser } from '../../actions/userAction'
+import {  useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchUser, logoutUser } from '../../actions/userAction'
 
 export default function UserDisplay() {
 
-    // const user = useSelector(state => state)
-    // const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
 
-    // useEffect(() =>{
-    //     dispatch(fetchUser())
-    // }, [])
+    
+    useEffect(() =>{
+        dispatch(fetchUser())
+    }, [])
+
+    function handleLogout(user){
+        dispatch(logoutUser(user))
+    }
+  
 
     return (
         <div>
             <h2>User</h2>
+            <h3>{user.username}</h3>
+            <button onClick={()=>(handleLogout(user))}>logout</button>
             
-            <ul>
-            
-            </ul>
+
         </div>
     )
 }
