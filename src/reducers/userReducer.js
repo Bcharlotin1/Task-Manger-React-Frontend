bbb04100324a13e49d91d0757ba1ea8edd75e627
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-11 09:21:56 -0500

    Removeed set user

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,11 +3,7 @@ import { SET_USER, GET_USER } from "../actions/constants";
 export default function userReducer(state={}, action) {
     
     switch(action.type){
-       
-        case SET_USER:
-           
-            return action.payload; 
-            
+          
         case GET_USER:
 
             return action.payload
